825916e27d330d6f3c3136b156cf6c7c31af214a
Rajeshroulo <rajraval017@gmail.com>
2020-05-05 10:32:56 +0530

    Added feature to calculate employee monthly wages

--- EmpWagecomputations.java
+++ EmpWagecomputations.java
@@ -1,23 +1,34 @@
 public class EmpWagecomputations {
 
-	public static void main(String []args){
+        private static final int IS_FULL_TIME = 1;
+	private static final int IS_PART_TIME = 2;
+	private static final int EMP_RATE_PER_HOUR = 20;
+	private static final int EMP_WORKING_DAYS = 20;
 
-                int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
+	public static void main(String []args){
 
 		int empHrs = 0;
 		int empWage = 0 ;
+		int totalEmpHrs = 0 ;
+		int totalEmpWage = 0 ;
 
-		double empCheck = Math.floor( Math.random() * 10 ) % 3;
-			if( empCheck == IS_FULL_TIME )
-				empHrs = 8;
-			else if( empCheck == IS_PART_TIME )
-				empHrs = 4;
-			else
-				empHrs = 0;
+		for(int day = 0; day < EMP_WORKING_DAYS ; day++ ){
+			int empCheck = (int) Math.floor( Math.random() * 10 ) % 3;
 
-			empWage = empHrs * EMP_RATE_PER_HOUR;
-			System.out.println("Employee Wage:"+ empWage);
+			switch( empCheck ){
+		 		case IS_FULL_TIME:
+					empHrs = 8;
+					break;
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+				default:
+					empHrs = 0;
+			}
+			totalEmpHrs += empHrs ;
+		}
+		totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage:"+ totalEmpWage);
 	}
+
 }
