dcf23202915877d5c2a28943269f1feb7db07222
Rajeshroulo <rajraval017@gmail.com>
2020-05-05 10:01:16 +0530

    Added feature to check employee attendance

---
+++ EmpWagecomputations.java
@@ -0,0 +1,12 @@
+public class EmpWagecomputations {
+
+	public static void main(String []args){
+
+		int IS_PRESENT = 1;
+		double empCheck = Math.floor( Math.random() * 10 ) % 2;
+			if( empCheck == IS_PRESENT )
+				System.out.println("Employee is present");
+			else
+				System.out.println("Employee is Absent");
+	}
+}
