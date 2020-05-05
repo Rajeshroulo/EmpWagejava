9744cc0dc81519a414aebd9356018df701e3b211
Rajeshroulo <rajraval017@gmail.com>
2020-05-05 10:20:46 +0530

    Added  feature to compute employee part time wages

--- EmpWagecomputations.java
+++ EmpWagecomputations.java
@@ -2,15 +2,18 @@ public class EmpWagecomputations {
 
 	public static void main(String []args){
 
-		int IS_PRESENT = 1;
+                int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
 
 		int empHrs = 0;
 		int empWage = 0 ;
 
-		double empCheck = Math.floor( Math.random() * 10 ) % 2;
-			if( empCheck == IS_PRESENT )
+		double empCheck = Math.floor( Math.random() * 10 ) % 3;
+			if( empCheck == IS_FULL_TIME )
 				empHrs = 8;
+			else if( empCheck == IS_PART_TIME )
+				empHrs = 4;
 			else
 				empHrs = 0;
 
